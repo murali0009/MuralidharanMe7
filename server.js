34bf373a06f6f33f1cf10f4be562bc4592c63be4
Muralidharan <mura0009@e.ntu.edu.sg>
2021-07-02 21:30:29 +0800

    redirect home

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
+app.get('/home', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
