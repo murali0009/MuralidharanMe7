81b1fd8e06ef15541fa4114a8c8b9e0ca3d73d0d
Muralidharan <mura0009@e.ntu.edu.sg>
2021-07-02 21:20:45 +0800

    Commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var helmet = require('helmet');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
+app.use(helmet());
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
